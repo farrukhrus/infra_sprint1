783fdb33c852c7c86dc333bb19a46b37dcff6540
Farrukh Rustamov <farruhrus@yandex.ru>
2024-01-27 20:13:57 +0000

    wtf3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,8 +6,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'dummy'
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','dummy')
 
 DEBUG = False
 
